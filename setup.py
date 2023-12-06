f3a739eac4fece506fc5c4a2267e77fa6e1cba1f
Josh Bickett <josh@bickett.net>
2023-12-06 06:37:21 -0800

    Add `long_description` to `setup`

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
+# Read the contents of your README.md file for the project description
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
 setup(
     name="self-operating-computer",
     version="1.0.2",
@@ -14,5 +18,7 @@ setup(
             "operate=operate.main:main_entry",
         ],
     },
+    long_description=long_description,  # Add project description here
+    long_description_content_type="text/markdown",  # Specify Markdown format
     # include any other necessary setup options here
 )
