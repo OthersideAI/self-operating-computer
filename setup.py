721dd420f7a16b636268421db82cf5d008abcf02
Josh Bickett <josh@bickett.net>
2023-12-06 05:10:12 -0800

    Add `open("requirements.txt")` to `setup.py` to avoid extra terminal statement

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your requirements.txt file
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
 
 setup(
     name="self-operating-computer",
     version="1.0.0",
     packages=find_packages(),
+    install_requires=required,  # Add dependencies here
     entry_points={
         "console_scripts": [
             "operate=operate.main:main_entry",
