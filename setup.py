40c176e918c1221279fa2ca8ca7db578cfd4d2a2
Josh Bickett <josh@bickett.net>
2023-12-12 18:52:07 -0800

    Iterate to`version="1.0.6"`

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as readme_file:
 
 setup(
     name="self-operating-computer",
-    version="1.0.5",
+    version="1.0.6",
     packages=find_packages(),
     install_requires=required,  # Add dependencies here
     entry_points={
