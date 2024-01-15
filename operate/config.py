849ce90fbde807f543a364e3520789a9c63945de
Josh Bickett <josh@bickett.net>
2024-01-15 11:13:33 -0800

    remove `print`

--- operate/config.py
+++ operate/config.py
@@ -22,13 +22,9 @@ class Config:
         self.google_api_key = os.getenv("GOOGLE_API_KEY")
 
     def initialize_openai(self):
-        print("[initialize_openai]")
-        print("[initialize_openai] self.openai_api_key", self.openai_api_key)
         if self.openai_api_key:
             client = OpenAI()
-            print("[initialize_openai] client = OpenAI()")
             client.api_key = self.openai_api_key
-            print("[initialize_openai] client.api_key", client.api_key)
             client.base_url = os.getenv("OPENAI_API_BASE_URL", client.base_url)
             return client
         return None
@@ -52,8 +48,6 @@ class Config:
         key_value = input_dialog(
             title="API Key Required", text=f"Please enter your {key_description}:"
         ).run()
-        print("[prompt_and_save_api_key]")
-        print("[prompt_and_save_api_key] key_value", key_value)
 
         if key_value is None:  # User pressed cancel or closed the dialog
             sys.exit("Operation cancelled by user.")
@@ -62,20 +56,13 @@ class Config:
             self.save_api_key_to_env(key_name, key_value)
             load_dotenv()  # Reload environment variables
             # Update the instance attribute with the new key
-            print("[prompt_and_save_api_key] key_name", key_name)
-            print("[prompt_and_save_api_key] key_value", key_value)
 
             if key_value:
                 self.save_api_key_to_env(key_name, key_value)
                 load_dotenv()  # Reload environment variables
                 setattr(self, key_name.lower(), key_value)
-            print("[prompt_and_save_api_key] self.openai_api_key ", self.openai_api_key)
-            print("[prompt_and_save_api_key] self.google_api_key ", self.google_api_key)
 
     @staticmethod
     def save_api_key_to_env(key_name, key_value):
-        print("[save_api_key_to_env]")
-        print("[save_api_key_to_env] key_name", key_name)
-        print("[save_api_key_to_env] key_value", key_value)
         with open(".env", "a") as file:
             file.write(f"\n{key_name}='{key_value}'")
