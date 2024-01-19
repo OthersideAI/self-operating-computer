657059f5c5fac563aac689b3e6c9139714a70a3b
Josh Bickett <josh@bickett.net>
2024-01-19 13:10:38 -0800

    Consolidate into only one `click` fucntion

--- operate/utils/operating_system.py
+++ operate/utils/operating_system.py
@@ -25,24 +25,13 @@ class OperatingSystem:
         except Exception as e:
             print("[OperatingSystem][press] error:", e)
 
-    def click(self, click_detail):
+    def click(self, click_detail, click_count=1):
         try:
             x = convert_percent_to_decimal(click_detail.get("x"))
             y = convert_percent_to_decimal(click_detail.get("y"))
 
             if click_detail and isinstance(x, float) and isinstance(y, float):
-                self.click_at_percentage(x, y)
-
-        except Exception as e:
-            print("[OperatingSystem][mouse] error:", e)
-
-    def double_click(self, click_detail):
-        try:
-            x = convert_percent_to_decimal(click_detail.get("x"))
-            y = convert_percent_to_decimal(click_detail.get("y"))
-
-            if click_detail and isinstance(x, float) and isinstance(y, float):
-                self.double_click_at_percentage(x, y)
+                self.click_at_percentage(x, y, click_count=click_count)
 
         except Exception as e:
             print("[OperatingSystem][mouse] error:", e)
@@ -51,6 +40,7 @@ class OperatingSystem:
         self,
         x_percentage,
         y_percentage,
+        click_count=1,
         duration=0.2,
         circle_radius=50,
         circle_duration=0.5,
@@ -69,32 +59,6 @@ class OperatingSystem:
                 y = y_pixel + math.sin(angle) * circle_radius
                 pyautogui.moveTo(x, y, duration=0.1)
 
-            pyautogui.click(x_pixel, y_pixel)
-        except Exception as e:
-            print("[OperatingSystem][click_at_percentage] error:", e)
-
-    def double_click_at_percentage(
-        self,
-        x_percentage,
-        y_percentage,
-        duration=0.2,
-        circle_radius=50,
-        circle_duration=0.5,
-    ):
-        try:
-            screen_width, screen_height = pyautogui.size()
-            x_pixel = int(screen_width * float(x_percentage))
-            y_pixel = int(screen_height * float(y_percentage))
-
-            pyautogui.moveTo(x_pixel, y_pixel, duration=duration)
-
-            start_time = time.time()
-            while time.time() - start_time < circle_duration:
-                angle = ((time.time() - start_time) / circle_duration) * 2 * math.pi
-                x = x_pixel + math.cos(angle) * circle_radius
-                y = y_pixel + math.sin(angle) * circle_radius
-                pyautogui.moveTo(x, y, duration=0.1)
-
-            pyautogui.click(clicks=2, x=x_pixel, y=y_pixel)
+            pyautogui.click(clicks=click_count, x=x_pixel, y=y_pixel)
         except Exception as e:
             print("[OperatingSystem][click_at_percentage] error:", e)
