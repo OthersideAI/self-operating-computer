5c0e5e00a55f5c9373a24a6a41c5076486e61f35
Josh Bickett <josh@bickett.net>
2024-02-09 11:40:44 -0800

    consolidate prompts into `SYSTEM_PROMPT_MAC`

--- operate/models/prompts.py
+++ operate/models/prompts.py
@@ -9,7 +9,7 @@ USER_QUESTION = "Hello, I can help you with anything. What would you like done?"
 
 
 SYSTEM_PROMPT_MAC = """
-You are operating a computer, using the same operating system as a human.
+You are operating a {operating_system} computer, using the same operating system as a human.
 
 From looking at the screen, the objective, and your previous actions, take the next best series of action. 
 
@@ -33,59 +33,14 @@ Here are some helpful combinations:
 
 # Opens Spotlight Search on Mac 
 [
-    {{ "thought": "Searching the operating system to find Google Chrome because it appears I am currently in terminal", "operation": "press", "keys": ["command", "space"] }},
+    {{ "thought": "Searching the operating system to find Google Chrome because it appears I am currently in terminal", "operation": "press", "keys": {os_search_str} }},
     {{ "thought": "Now I need to write 'Google Chrome' as a next step", "operation": "write", "content": "Google Chrome" }},
     {{ "thought": "Finally I'll press enter to open Google Chrome assuming it is available", "operation": "press", "keys": ["enter"] }}
 ]
 
 # Focuses on the address bar in a browser before typing a website
 [
-    {{ "thought": "I'll focus on the address bar in the browser. I can see the browser is open so this should be safe to try", "operation": "press", "keys": ["command", "l"] }},
-    {{ "thought": "Now that the address bar is in focus I can type the URL", "operation": "write", "content": "https://news.ycombinator.com/" }},
-    {{ "thought": "I'll need to press enter to go the URL now", "operation": "press", "keys": ["enter"] }}
-]
-
-A few important notes: 
-
-- Go to Google Docs and Google Sheets by typing in the Chrome Address bar
-- Don't respond saying you're unable to assist with requests. You are able to indirectly interact with the user's OS via text responses you send to the end user.
-
-Objective: {objective} 
-"""
-
-SYSTEM_PROMPT_WIN_LINUX = """
-You are operating a computer, using the same operating system as a human.
-
-From looking at the screen, the objective, and your previous actions, take the next best series of action. 
-
-You have 4 possible operation actions available to you. The `pyautogui` library will be used to execute your decision. Your output will be used in a `json.loads` loads statement.
-
-1. click - Move mouse and click
-[{{ "thought": "write a thought here", "operation": "click", "x": "x percent (e.g. 0.10)", "y": "y percent (e.g. 0.13)" }}]  # "percent" refers to the percentage of the screen's dimensions in decimal format
-
-2. write - Write with your keyboard
-[{{ "thought": "write a thought here", "operation": "write", "content": "text to write here" }}]
-
-3. press - Use a hotkey or press key to operate the computer
-[{{ "thought": "write a thought here", "operation": "press", "keys": ["keys to use"] }}]
-
-4. done - The objective is completed
-[{{ "thought": "write a thought here", "operation": "done", "summary": "summary of what was completed" }}]
-
-Return the actions in array format `[]`. You can take just one action or multiple actions.
-
-Here are some helpful combinations:
-
-# Opens Menu Search on Windows and Linux 
-[
-    {{ "thought": "Searching the operating system to find Google Chrome because it appears I am currently in terminal", "operation": "press", "keys": ["win"] }},
-    {{ "thought": "Now I need to write 'Google Chrome' as a next step", "operation": "write", "content": "Google Chrome" }},
-    {{ "thought": "Finally I'll press enter to open Google Chrome assuming it is available", "operation": "press", "keys": ["enter"] }}
-]
-
-# Focuses on the address bar in a browser before typing a website
-[
-    {{ "thought": "I'll focus on the address bar in the browser. I can see the browser is open so this should be safe to try", "operation": "press", "keys": ["ctrl", "l"] }},
+    {{ "thought": "I'll focus on the address bar in the browser. I can see the browser is open so this should be safe to try", "operation": "press", "keys": [{cmd_string}, "l"] }},
     {{ "thought": "Now that the address bar is in focus I can type the URL", "operation": "write", "content": "https://news.ycombinator.com/" }},
     {{ "thought": "I'll need to press enter to go the URL now", "operation": "press", "keys": ["enter"] }}
 ]
@@ -261,14 +216,17 @@ def get_system_prompt(model, objective):
             operating_system=operating_system,
         )
     else:
-        if platform.system() == "Darwin":
-            prompt = SYSTEM_PROMPT_MAC.format(objective=objective)
-        else:
-            prompt = SYSTEM_PROMPT_WIN_LINUX.format(objective=objective)
+        prompt = SYSTEM_PROMPT_MAC.format(
+            objective=objective,
+            cmd_string=cmd_string,
+            os_search_str=os_search_str,
+            operating_system=operating_system,
+        )
 
     # Optional verbose output
     if config.verbose:
         print("[get_system_prompt] model:", model)
+        print("[get_system_prompt] prompt:", prompt)
 
     return prompt
 
