2d8b56e52fc7c26b096d6bfebade5538908baa4e
Tarek ElMalah <telmalah@freedommobile.ca>
2025-07-06 17:43:15 -0400

    chore: Update version to 2.0.4 in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as readme_file:
 
 setup(
     name="self-ai-operating-computer",
-    version="1.5.8",
+    version="2.0.4",
     packages=find_packages(),
     install_requires=required,  # Add dependencies here
     entry_points={
