483536ba0aa3c3fe887d45c7ac3060389dadda4c
Tarek ElMalah <telmalah@freedommobile.ca>
2025-07-07 10:22:31 -0400

    chore: Update version to 2.0.13 in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as readme_file:
 
 setup(
     name="self-ai-operating-computer",
-    version="2.0.12",
+    version="2.0.13",
     packages=find_packages(),
     install_requires=[
         "annotated-types==0.6.0",
